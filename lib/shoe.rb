5f079e87d8d418414df8a4f65dd9345f00a62b2d
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-22 23:47:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,12 @@ class Book
 
 attr_accessor :author, :page_count, :genre 
 
-def initialize(title)
-  @title = title 
+def initialize(brand)
+  @brand = brand  
 end 
 
-def title 
-  @title 
+def color 
+  @color 
 end 
 
 def turn_page 
